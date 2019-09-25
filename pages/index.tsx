67647439347c5be734d3eb74681379d820aacaa2
Ari Mendelow <ari@mendelow.me>
2019-09-24 20:47:40 -0700

    Create a PostLink component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,20 @@ import Link from 'next/link';
 
 import Layout from '../components/Layout';
 
+type Props = {
+	title?: string
+};
+
+const PostLink: React.FunctionComponent<Props> = ({
+	title = 'default title',
+}) => (
+	<li>
+		<Link href={`/post?title=${title}`}>
+			<a>{title}</a>
+		</Link>
+	</li>
+);
+
 const Index = () => (
 	<p>Hello Next.js</p>
 );
