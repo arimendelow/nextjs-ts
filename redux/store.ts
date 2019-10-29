498f96c0f29dc33253e1bad10b01804368c055ad
Ari Mendelow <ari@mendelow.me>
2019-10-29 14:08:40 -0700

    Define the store

---
+++ redux/store.ts
@@ -0,0 +1,19 @@
+// We define the one store so that we can access any states from the store
+import thunkMiddleware from 'redux-thunk';
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+  Store as ReduxStore,
+} from 'redux';
+import { createLogger } from 'redux-logger';
+import reducers, { initialState } from './reducers';
+
+const dev: boolean = process.env.NODE_ENV !== 'production';
+
+export type Store = ReduxStore<typeof initialState>;
+
+export default (state = initialState): Store => {
+  const middlewares = dev ? [thunkMiddleware, createLogger()] : [];
+  return createStore(reducers, state, compose(applyMiddleware(...middlewares)));
+};
