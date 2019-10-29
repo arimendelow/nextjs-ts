df5d3c4ab735d914f77f76a724eed42ceeaa5f7d
Ari Mendelow <ari@mendelow.me>
2019-10-29 13:53:08 -0700

    Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,16 @@ import * as React from 'react';
 import { NextPage } from 'next';
 import fetch from 'isomorphic-unfetch';
 
+import '../styles/tailwind.src.css';
+
 type Props = {};
 
 const Index: NextPage<Props> = ({
 	// Props go here
 }) => (
-	<h1>Hello, world!</h1>
+	<div className="h-screen w-screen table">
+		<h1 className="text-5xl text-center table-cell align-middle">Hello, world!</h1>
+	</div>
 );
 
 
