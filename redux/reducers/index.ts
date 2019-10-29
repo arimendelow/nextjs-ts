4e213c6f8540863dd97d2349e7f4257da64fa13e
Ari Mendelow <ari@mendelow.me>
2019-10-29 14:06:50 -0700

    Create the file for combining the reducers

---
+++ redux/reducers/index.ts
@@ -0,0 +1,11 @@
+// This combines all reducers
+import { combineReducers } from 'redux';
+import todo, { initialState as todoState } from './todo';
+
+export const initialState = {
+  todo: todoState,
+};
+
+export default combineReducers({
+  todo,
+});
