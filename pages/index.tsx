c06e4c7dd44e0a9df4224add2afcbba67fed389c
Ari Mendelow <ari@mendelow.me>
2019-10-29 13:51:02 -0700

    Clear up file structure to make way for new project

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,45 +4,13 @@ import * as React from 'react';
 import { NextPage } from 'next';
 import fetch from 'isomorphic-unfetch';
 
-import Layout from '../components/Layout';
-
-type show = {
-	id: string,
-	name: string
-}
-type Props = {
-	shows: Array<show>;
-};
+type Props = {};
 
 const Index: NextPage<Props> = ({
-	shows = []
+	// Props go here
 }) => (
-	<Layout>
-		<h1 className="font-bold text-5xl pb-5 text-center">Batman TV Shows</h1>
-		<ul className="text-center">
-			{shows.map(show => (
-				<li key={show.id} className="font-bold p-2 text-xl text-blue-900 underline">
-					<Link href="/p/[id]" as={`/p/${show.id}`}>
-						<a>{show.name}</a>
-					</Link>
-				</li>
-			))}
-		</ul>
-	</Layout>
+	<h1>Hello, world!</h1>
 );
 
-Index.getInitialProps = async function() {
-	const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-	const data = await res.json();
-	
-	console.log(`Show data fetched. Count: ${data.length}`);
-
-	return {
-		shows: data.map((entry: {show: object}) => {
-			const { show } = entry || {}
-			return show
-		})
-	};
-};
 
 export default Index;
